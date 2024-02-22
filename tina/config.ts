bbd4439b7dc0b67ad7345a5d51c616fe0d967ef5
harold.preyers@vconsultants.be <harold.preyers@vconsultants.be>
2024-02-22 15:50:59 +0100

    TinaCMS

--- tina/config.ts
+++ tina/config.ts
@@ -49,7 +49,7 @@ export default defineConfig({
         ],
         ui: {
           // This is an DEMO router. You can remove this to fit your site
-          router: ({ document }) => `/demo/blog/${document._sys.filename}`,
+          router: ({ document }) => `/${document._sys.filename}`,
         },
       },
     ],
